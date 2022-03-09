34b434699a952726eaf6840acd2705e35e2a5db3
AlexV <v.superalex@gmail.com>
2022-03-09 16:08:11 +0300

    91_CRA

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+const Header = () => {
+  return <h2>Hello!</h2>
+}
+
+const Field = () => {
+  return <input placeholder="Вводите" type="text"/>
+}
+
+function Btn() {
+  const text ='Log in';
+  return <button>{text}</button>
+}
+
+
+function WhoAmI (props) {
+  return (
+    <div>
+      <h1>My name is {props.name}, surname - {props.surname}</h1>
+      <a href={props.link}>My profile</a>
+    </div>
+  )
+}
+
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <WhoAmI name="Alex" surname="Voitka" link="pumba.com"/>
+      <WhoAmI name="John" surname="Rembo" link="pumba-1.com"/>
     </div>
   );
 }
+
+
 
 export default App;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,22 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-// const elem = <h2>Hi, Alex!</h2>;
-// const hi = React.createElement('h2', {className: 'item'}, 'Hi, world!');
 
-const text = 'World';
-
-const elem = (
-    <div>
-      <h2>Hello {text}</h2>
-      <input type="text"/>
-      <button/>
-      <button>click</button>
-    </div>
-);
 
 ReactDOM.render(
-  elem,
+  <App/>,
   document.getElementById('root')
 );
 
